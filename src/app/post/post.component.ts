26f7a19167990c9b7e4b48baf90ea87708e26d7b
Admin <nazarkharechko90@gmail.com>
2020-04-09 22:39:32 +0300

    lesson2

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ModolPost} from '../modolPost/modolPost';
 
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
